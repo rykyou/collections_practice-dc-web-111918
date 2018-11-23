a9ab7b490244d530cf4a98607954bc15836f78cd
Robin Kim <rkim1214@gmail.com>
2018-11-23 18:37:31 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -8,8 +8,12 @@ def sort_array_desc(integers)
   end                       #or shorter: integers.sort.reverse
 end 
 
-def sort_array_char_count (strings_array)
+def sort_array_char_count(strings_array)
   strings_array.sort do |a,b|
     a.length <=> b.length 
   end 
+end 
+
+def swap_elements(array)
+  
 end 
